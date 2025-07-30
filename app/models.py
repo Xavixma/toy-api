eb1b3618820f39d082848e4499222f5e316b5b45
Xavi Martin <xavi.xma@gmail.com>
2025-07-30 14:22:44 +0200

    multitenancy schema added

--- app/models.py
+++ app/models.py
@@ -5,3 +5,5 @@ class Item(db.Model):
     __tablename__ = "items"
     id = db.Column(Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    tenant_id = db.Column(db.String(50), nullable=False)
+
